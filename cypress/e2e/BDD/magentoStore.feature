32a6308ac8018ad54c1713980986a23d1d70ae38
wojciacho <warrockpremk@o2.pl>
2023-03-30 18:24:37 +0200

    validating woman page

--- cypress/e2e/BDD/magentoStore.feature
+++ cypress/e2e/BDD/magentoStore.feature
@@ -6,3 +6,8 @@ When Page loads check navbar
 And Check women page
 Then Go to home page
 
+Scenario: magentoSoftware - check woman page
+Given Opening magentoSoftware page
+When Page loads check navbar
+And Go to women page and assert
+Then Go to home page
--- cypress/e2e/BDD/magentoStore/magentoStore.js
+++ cypress/e2e/BDD/magentoStore/magentoStore.js
@@ -2,28 +2,48 @@ import {Given, When, Then} from "@badeball/cypress-cucumber-preprocessor";
 import magentoStore from "../../../page-objects/magentoStoreObject";
 
 Given("Opening magentoSoftware page", () => {
-    magentoStore.navigate()
-    cy.url().should("include", "magento.softwaretestingboard.com")
-})
+  magentoStore.navigate();
+  cy.url().should("include", "magento.softwaretestingboard.com");
+});
 
 When("Page loads check navbar", () => {
-    cy.get("#ui-id-3 > span").should("be.visible")
-    cy.get("#ui-id-4 > span:nth-child(2)").should("be.visible")
-    cy.get("#ui-id-5 > span:nth-child(2)").should("be.visible")
-    cy.get("#ui-id-6 > span:nth-child(2)").should("be.visible")
-    cy.get("#ui-id-7 > span:nth-child(2)").should("be.visible")
-    cy.get("#ui-id-8 > span").should("be.visible")
-})
+  cy.get("#ui-id-3 > span").should("be.visible");
+  cy.get("#ui-id-4 > span:nth-child(2)").should("be.visible");
+  cy.get("#ui-id-5 > span:nth-child(2)").should("be.visible");
+  cy.get("#ui-id-6 > span:nth-child(2)").should("be.visible");
+  cy.get("#ui-id-7 > span:nth-child(2)").should("be.visible");
+  cy.get("#ui-id-8 > span").should("be.visible");
+});
 
 When("Check women page", () => {
-    cy.get("#ui-id-4 > span:nth-child(2)").click()
-    cy.url().should("include", "/women.html")
-    cy.get("#page-title-heading > span").should("contain", "Women")
-})
+  cy.get("#ui-id-4 > span:nth-child(2)").click();
+  cy.url().should("include", "/women.html");
+  cy.get("#page-title-heading > span").should("contain", "Women");
+});
 
-
+When("Go to women page and assert", () => {
+  cy.get("#ui-id-4 > span:nth-child(2)").click();
+  cy.get("#page-title-heading > span").should("contain", "Women");
+  cy.get(".sidebar.sidebar-main").should("be.visible");
+  cy.get("div > ol > li:nth-child(1) > div > div > strong").should(
+    "contain",
+    "Radiant Tee"
+  );
+  cy.get("div > ol > li:nth-child(2) > div > div > strong").should(
+    "contain",
+    "Breathe-Easy Tank"
+  );
+  cy.get("div > ol > li:nth-child(3) > div > div > strong").should(
+    "contain",
+    "Selene Yoga Hoodie"
+  );
+  cy.get("div > ol > li:nth-child(4) > div > div > strong").should(
+    "contain",
+    "Deirdre Relaxed-Fit Capri"
+  );
+});
 
 Then("Go to home page", () => {
-    cy.get(".logo").click()
-    cy.url().should("contain", "magento.softwaretestingboard.com")
-})
+  cy.get(".logo").click();
+  cy.url().should("contain", "magento.softwaretestingboard.com");
+});
